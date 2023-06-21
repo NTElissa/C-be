5486aa92e7b3b025f82f7c5f2a3e03703b617119
NTElissa <ntihindukaelissa77@gmail.com>
2023-06-21 13:43:09 +0200

    end--no-edit

--- dist/controllers/userController.js
+++ dist/controllers/userController.js
@@ -4,11 +4,10 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.default = void 0;
-var _models = require("..models/models");
+var _models = require("../database/models.js");
 var _express = _interopRequireDefault(require("express"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 const app = (0, _express.default)();
-app.use(_express.default.json());
 const createUser = async (req, res) => {
   const {
     firstName,
